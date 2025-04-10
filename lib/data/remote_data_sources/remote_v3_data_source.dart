7a35f2977413f777e9cd3273758aa32541076be4
ming.chen <ming.chen.02030925@gmail.com>
2025-04-10 10:13:48 +0800

    Refactor SpaceX data handling to improve type safety and mapping
    - Updated RemoteV3DataSource to return a list of LaunchesDto, enhancing type safety in API responses.
    - Refactored SpaceXRepositoryImpl to map LaunchesDto to SpaceXFlight, improving data representation.
    - Adjusted SpaceXRepository interface to reflect the new return type of List<SpaceXFlight>.
    - Enhanced GetSpaceXLaunchesUsecase to handle the new data structure, ensuring consistent data flow.
    - Updated unit tests to validate the new data handling and mapping logic, ensuring robustness and correctness.

--- lib/data/remote_data_sources/remote_v3_data_source.dart
+++ lib/data/remote_data_sources/remote_v3_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:newton_tech_interview_quiz/data/dtos/launches_dto.dart';
 import 'package:newton_tech_interview_quiz/services/api_service.dart';
 
 class RemoteV3DataSource {
@@ -6,11 +7,13 @@ class RemoteV3DataSource {
   RemoteV3DataSource({ApiService? apiService})
       : apiService = apiService ?? ApiService();
 
-  Future<dynamic> getLaunches() async {
+  Future<List<LaunchesDto>> getLaunches() async {
     final response = await apiService.get('/v3/launches');
     final code = response.statusCode;
     return switch (code) {
-      200 => response.data,
+      200 => (response.data as List<dynamic>)
+          .map((launchdata) => LaunchesDto.fromJson(launchdata))
+          .toList(),
       _ => throw Exception('api error'),
     };
   }
--- test/data/repositories/spacex_repository_impl_test.dart
+++ test/data/repositories/spacex_repository_impl_test.dart
@@ -4,9 +4,9 @@ import 'package:mockito/mockito.dart';
 import 'package:newton_tech_interview_quiz/data/dtos/launches_dto.dart';
 import 'package:newton_tech_interview_quiz/data/remote_data_sources/remote_v3_data_source.dart';
 import 'package:newton_tech_interview_quiz/data/repositories/spacex_repository_impl.dart';
+import 'package:newton_tech_interview_quiz/domain/value_objects/spacex_flight.dart';
 
 import 'spacex_repository_impl_test.mocks.dart';
-
 
 @GenerateMocks([RemoteV3DataSource])
 void main() {
@@ -14,123 +14,117 @@ void main() {
   late SpaceXRepositoryImpl spaceXRepositoryImpl;
   setUp(() {
     remoteV3DataSource = MockRemoteV3DataSource();
-    spaceXRepositoryImpl = SpaceXRepositoryImpl(remoteV3DataSource: remoteV3DataSource);
+    spaceXRepositoryImpl =
+        SpaceXRepositoryImpl(remoteV3DataSource: remoteV3DataSource);
   });
   test('get launches empty list...', () async {
-    when(remoteV3DataSource.getLaunches()).thenAnswer((_) async => []);
+    when(remoteV3DataSource.getLaunches()).thenAnswer((_) async => <LaunchesDto>[]);
     final launches = await spaceXRepositoryImpl.getLaunches();
-    expect(launches, isA<List<LaunchesDto>>());
+    expect(launches, isA<List<SpaceXFlight>>());
   });
   test('get launches with data...', () async {
     when(remoteV3DataSource.getLaunches()).thenAnswer((_) async => [
-      {
-        "flight_number": 110,
-        "mission_name": "CRS-21", 
-        "mission_id": ["EE86F74"],
-        "launch_year": "2020",
-        "launch_date_unix": 1607271420,
-        "launch_date_utc": "2020-12-06T16:17:00.000Z",
-        "launch_date_local": "2020-12-06T11:17:00-05:00",
-        "is_tentative": false,
-        "tentative_max_precision": "hour",
-        "tbd": false,
-        "launch_window": null,
-        "rocket": {
-          "rocket_id": "falcon9",
-          "rocket_name": "Falcon 9",
-          "rocket_type": "FT",
-          "first_stage": {
-            "cores": [
-              {
-                "core_serial": "B1058",
-                "flight": 4,
-                "block": 5,
-                "gridfins": true,
-                "legs": true,
-                "reused": true,
-                "land_success": null,
-                "landing_intent": true,
-                "landing_type": "ASDS",
-                "landing_vehicle": null
-              }
-            ]
-          },
-          "second_stage": {
-            "block": 5,
-            "payloads": [
-              {
-                "payload_id": "CRS-21",
-                "norad_id": [],
-                "cap_serial": "C208", 
-                "reused": true,
-                "customers": ["NASA (CRS)"],
-                "nationality": "United States",
-                "manufacturer": "SpaceX",
-                "payload_type": "Dragon 2.0",
-                "payload_mass_kg": null,
-                "payload_mass_lbs": null,
-                "orbit": "ISS",
-                "orbit_params": {
-                  "reference_system": "geocentric",
-                  "regime": "low-earth",
-                  "longitude": null,
-                  "semi_major_axis_km": null,
-                  "eccentricity": null,
-                  "periapsis_km": null,
-                  "apoapsis_km": null,
-                  "inclination_deg": null,
-                  "period_min": null,
-                  "lifespan_years": null,
-                  "epoch": null,
-                  "mean_motion": null,
-                  "raan": null,
-                  "arg_of_pericenter": null,
-                  "mean_anomaly": null
-                },
-                "mass_returned_kg": null,
-                "mass_returned_lbs": null,
-                "flight_time_sec": null,
-                "cargo_manifest": null
-              }
-            ]
-          },
-          "fairings": null
-        },
-        "ships": [],
-        "telemetry": {
-          "flight_club": null
-        },
-        "launch_site": {
-          "site_id": "ksc_lc_39a",
-          "site_name": "KSC LC 39A",
-          "site_name_long": "Kennedy Space Center Historic Launch Complex 39A"
-        },
-        "launch_success": null,
-        "links": {
-          "mission_patch": "https://imgur.com/E7fjUBD.png",
-          "mission_patch_small": "https://imgur.com/jHNFSY6.png",
-          "reddit_campaign": "https://www.reddit.com/r/spacex/comments/jw8bfe/crs21_launch_campaign_thread/",
-          "reddit_launch": null,
-          "reddit_recovery": null,
-          "reddit_media": null,
-          "presskit": null,
-          "article_link": null,
-          "wikipedia": null,
-          "video_link": null,
-          "youtube_id": null,
-          "flickr_images": []
-        },
-        "details": "SpaceX's 21st ISS resupply mission on behalf of NASA and the first under the CRS-2 contract, this mission brings essential supplies to the International Space Station using the cargo variant of SpaceX's Dragon 2 spacecraft. The external payload for this mission is the Nanoracks Bishop Airlock. Falcon 9 and Dragon launch from LC-39A, Kennedy Space Center and the booster is expected to land on an ASDS. The mission will be complete with return and recovery of the Dragon capsule and down cargo.",
-        "upcoming": true,
-        "static_fire_date_utc": null,
-        "static_fire_date_unix": null,
-        "timeline": null,
-        "crew": null
-      }
-    ]);
+          LaunchesDto(
+              flightNumber: 110,
+              missionName: 'CRS-21',
+              missionId: ['EE86F74'],
+              upcoming: true,
+              launchYear: '2020',
+              launchDateUnix: 1607271420,
+              launchDateUtc: '2020-12-06T16:17:00.000Z',
+              launchDateLocal: '2020-12-06T11:17:00-05:00',
+              isTentative: false,
+              tentativeMaxPrecision: 'hour',
+              tbd: false,
+              launchWindow: null,
+              rocket: RocketDto(
+                  rocketId: 'falcon9',
+                  rocketName: 'Falcon 9',
+                  rocketType: 'FT',
+                  firstStage: FirstStageDto(cores: [
+                    CoreDto(
+                        coreSerial: 'B1058',
+                        flight: 4,
+                        block: 5,
+                        gridfins: true,
+                        legs: true,
+                        reused: true,
+                        landSuccess: null,
+                        landingIntent: true,
+                        landingType: 'ASDS',
+                        landingVehicle: null)
+                  ]),
+                  secondStage: SecondStageDto(block: 5, payloads: [
+                    PayloadDto(
+                      payloadId: 'CRS-21',
+                      noradId: [],
+                      reused: true,
+                      customers: ['NASA (CRS)'],
+                      nationality: 'United States',
+                      manufacturer: 'SpaceX',
+                      payloadType: 'Dragon 2.0',
+                      payloadMassKg: null,
+                      payloadMassLbs: null,
+                      orbit: 'ISS',
+                      orbitParams: OrbitParamsDto(
+                          referenceSystem: 'geocentric',
+                          regime: 'low-earth',
+                          longitude: null,
+                          semiMajorAxisKm: null,
+                          eccentricity: null,
+                          periapsisKm: null,
+                          apoapsisKm: null,
+                          inclinationDeg: null,
+                          periodMin: null,
+                          lifespanYears: null,
+                          epoch: null,
+                          meanMotion: null,
+                          raan: null,
+                          argOfPericenter: null,
+                          meanAnomaly: null),
+                    )
+                  ]),
+                  fairings: null),
+              ships: [],
+              telemetry: TelemetryDto(flightClub: null),
+              launchSite: LaunchSiteDto(
+                  siteId: 'ksc_lc_39a',
+                  siteName: 'KSC LC 39A',
+                  siteNameLong:
+                      'Kennedy Space Center Historic Launch Complex 39A'),
+              launchSuccess: null,
+              launchFailureDetails: null,
+              links: LinksDto(
+                  missionPatch: 'https://imgur.com/E7fjUBD.png',
+                  missionPatchSmall: 'https://imgur.com/jHNFSY6.png',
+                  redditCampaign:
+                      'https://www.reddit.com/r/spacex/comments/jw8bfe/crs21_launch_campaign_thread/',
+                  redditLaunch: null,
+                  redditRecovery: null,
+                  redditMedia: null,
+                  presskit: null,
+                  articleLink: null,
+                  wikipedia: null,
+                  videoLink: null,
+                  youtubeId: null,
+                  flickrImages: []),
+              details:
+                  'SpaceX\'s 21st ISS resupply mission on behalf of NASA and the first under the CRS-2 contract, this mission brings essential supplies to the International Space Station using the cargo variant of SpaceX\'s Dragon 2 spacecraft. The external payload for this mission is the Nanoracks Bishop Airlock. Falcon 9 and Dragon launch from LC-39A, Kennedy Space Center and the booster is expected to land on an ASDS. The mission will be complete with return and recovery of the Dragon capsule and down cargo.',
+              staticFireDateUtc: null,
+              staticFireDateUnix: null,
+              timeline: null,
+              crew: null)
+        ]);
     final launches = await spaceXRepositoryImpl.getLaunches();
-    expect(launches, isA<List<LaunchesDto>>());
+    expect(launches, isA<List<SpaceXFlight>>());
+    expect(launches.length, 1);
+    expect(launches[0].flightNumber, 110);
+    expect(launches[0].missionName, 'CRS-21');
+    expect(launches[0].launchDate,
+        DateTime.fromMillisecondsSinceEpoch(1607271420 * 1000));
+    expect(launches[0].articleLink, null);
+    expect(launches[0].wikiLink, null);
+    expect(launches[0].videoLink, null);
+    expect(launches[0].iconPath, 'https://imgur.com/E7fjUBD.png');
   });
 }
-
-
